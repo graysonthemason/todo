4cbfdc05d661175deebb1022506d4af388e0cf9c
Gray Reinhard <gray.reinhard@gmail.com>
2016-03-16 17:29:24 -0400

    migrate to typescript based development environment

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 <h1>ToDo App</h1>
+<h3>npm scripts</h3>
+<ul>
+  <li>npm start - run the compiler and a server at the same time, both in "watch mode"</li>
+  <li>npm run tsc - run the TypeScript compiler once</li>
+  <li>npm run tsc:w - run the TypeScript compiler in watch mode; the process keeps running, awaiting changes to TypeScript files and re-compiling when it sees them.</li>
+  <li>npm run typings - runs the typings tool</li>
+  <li>npm postinstall - called by npm automatically after it successfully completes package installation. This script installs the TypeScript definition files this app requires.</li>
+</ul>
--- app/main.js
+++ app/main.js
@@ -1,9 +1,18 @@
-// wrap all components in an imediately invoked function to keep global namespace clean
-(function(app) {
-  document.addEventListener('DOMContentLoaded', function() {
-    // we use the bootstrap function from ng.platform.browser instead of ng.core
-    // so we can bootstrap this on other non-browser applications some day
-    // basically on document load, we run our main App component.
-    ng.platform.browser.bootstrap(app.AppComponent);
-  });
-})(window.app || (window.app = {}));
+System.register(['angular2/platform/browser', './app.component'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var browser_1, app_component_1;
+    return {
+        setters:[
+            function (browser_1_1) {
+                browser_1 = browser_1_1;
+            },
+            function (app_component_1_1) {
+                app_component_1 = app_component_1_1;
+            }],
+        execute: function() {
+            browser_1.bootstrap(app_component_1.AppComponent);
+        }
+    }
+});
+//# sourceMappingURL=main.js.map
